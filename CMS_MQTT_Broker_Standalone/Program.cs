85e07ac6fa3dd03e19b3b0d72e29e022d14396ff
Mooneer Salem <mooneer@gmail.com>
2021-03-02 23:59:29 -0800

    Remove deprecated project + add project to allow standalone exec.

---
+++ CMS_MQTT_Broker_Standalone/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using winlink.cms.mqtt;
+
+namespace CMS_MQTT_Broker_Standalone
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var broker = new MirroringMqttBroker();
+            var cancelToken = new CancellationToken();
+            await broker.StartAsync(cancelToken);
+        }
+    }
+}
