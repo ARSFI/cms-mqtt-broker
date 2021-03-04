2d5d5de5b4c265cf6b01c6d4494b0f4949899da2
Mooneer Salem <mooneer@gmail.com>
2021-03-04 01:12:19 -0800

    Update standalone project to use CMS database classes.

--- CMS_MQTT_Broker_Standalone/Program.cs
+++ CMS_MQTT_Broker_Standalone/Program.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using winlink.cms.mqtt;
 using winlink.cms.mqtt.config;
+using winlink.cms.data;
 
 namespace CMS_MQTT_Broker_Standalone
 {
@@ -10,7 +11,15 @@ namespace CMS_MQTT_Broker_Standalone
     {
         static async Task Main(string[] args)
         {
-            var broker = new MirroringMqttBroker(new ServiceConfigurationFactory().GetConfiguration());
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: {0} [SQL Config String]", args[0]);
+                return;
+            }
+
+            var database = new CMSDatabase(args[1]);
+            var propertiesTable = new CMSProperties(database);
+            var broker = new MirroringMqttBroker(new DatabaseServiceConfiguration(propertiesTable));
             var cancelToken = new CancellationToken();
             await broker.StartAsync(cancelToken);
         }
