ebbd1d2bb8ca76bfd0e4e194736d4a2abb731221
Lee Inman, K0QED <lee@iqed.net>
2021-03-30 13:02:34 -0600

    more tweaks to modified mqtt example

--- MQTTnet.Server/Program.cs
+++ MQTTnet.Server/Program.cs
@@ -14,8 +14,6 @@ namespace MQTTnet.Server
         {
             try
             {
-                PrintLogo();
-
                 Host.CreateDefaultBuilder(args)
                     .ConfigureWebHostDefaults(webBuilder =>
                     {
@@ -35,46 +33,5 @@ namespace MQTTnet.Server
             }
         }
 
-        static void PrintLogo()
-        {
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Red;
-            const string LogoText =
-@"
-
-███╗   ███╗ ██████╗ ████████╗████████╗███╗   ██╗███████╗████████╗    ███████╗███████╗██████╗ ██╗   ██╗███████╗██████╗ 
-████╗ ████║██╔═══██╗╚══██╔══╝╚══██╔══╝████╗  ██║██╔════╝╚══██╔══╝    ██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
-██╔████╔██║██║   ██║   ██║      ██║   ██╔██╗ ██║█████╗     ██║       ███████╗█████╗  ██████╔╝██║   ██║█████╗  ██████╔╝
-██║╚██╔╝██║██║▄▄ ██║   ██║      ██║   ██║╚██╗██║██╔══╝     ██║       ╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██╔══╝  ██╔══██╗
-██║ ╚═╝ ██║╚██████╔╝   ██║      ██║   ██║ ╚████║███████╗   ██║       ███████║███████╗██║  ██║ ╚████╔╝ ███████╗██║  ██║
-╚═╝     ╚═╝ ╚══▀▀═╝    ╚═╝      ╚═╝   ╚═╝  ╚═══╝╚══════╝   ╚═╝       ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
-                                                                                                                      
-";
-
-            Console.WriteLine(LogoText);
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("The official MQTT server implementation of MQTTnet");
-            Console.WriteLine("Copyright (c) 2017-2020 The MQTTnet Team");
-            Console.WriteLine(@"https://github.com/chkr1011/MQTTnet");
-
-            Console.ForegroundColor = ConsoleColor.White;
-
-            var fileVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
-
-            Console.WriteLine($@"
-Version:    {fileVersion.ProductVersion}
-License:    MIT (read LICENSE file)
-Sponsoring: https://opencollective.com/mqttnet
-Support:    https://github.com/chkr1011/MQTTnet/issues
-Docs:       https://github.com/chkr1011/MQTTnet/wiki/MQTTnetServer
-");
-
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(" ! THIS IS AN ALPHA VERSION! IT IS NOT RECOMMENDED TO USE IT FOR ANY DIFFERENT PURPOSE THAN TESTING OR EVALUATING!");
-            Console.ResetColor();
-            Console.WriteLine();
-        }
     }
 }
